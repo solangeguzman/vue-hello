19ad2eb0a7070fe0fddd62d8013b1bc7a61a2d28
solangeguzman <solangeguzmanduchen@gmail.com>
2021-06-24 14:12:09 +0200

    codice+bonus dentro

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+// Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const app = new Vue(
+    {
+        el: "#app",
+        data: {
+            text: 'ciao, piacere di averti nella mia pagina',
+        }
+    }
+
+);
+
+// BONUS
+// Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+const img = new Vue(
+    {
+        el: "#img",
+        data: {
+            Image: 'https://www.netfarm.it/web/image/4973/vuejs.png',
+        }
+    }
+
+);
